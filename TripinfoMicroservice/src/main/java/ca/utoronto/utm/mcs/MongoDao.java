7c212fec12b3048d31e697ad90bd13340761b372
Jeremy La <jeremylai511@gmail.com>
2022-11-20 19:55:29 -0500

    Set up MongoDB connection

--- TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/MongoDao.java
+++ TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/MongoDao.java
@@ -1,6 +1,8 @@
 package ca.utoronto.utm.mcs;
 
+import com.mongodb.client.*;
 import com.mongodb.client.MongoCollection;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.bson.Document;
 
 public class MongoDao {
@@ -8,9 +10,16 @@ public class MongoDao {
 	public MongoCollection<Document> collection;
 
 	public MongoDao() {
-        // TODO: 
-        // Connect to the mongodb database and create the database and collection. 
-        // Use Dotenv like in the DAOs of the other microservices.
+		Dotenv dotenv = Dotenv.load();
+		String addr = dotenv.get("MONGODB_ADDR");
+		String username = dotenv.get("MONGO_INITDB_ROOT_USERNAME");
+		String password = dotenv.get("MONGO_INITDB_ROOT_PASSWORD");
+		String uriDb = String.format("mongodb://%s:%s@%s:27017", username, password, addr);
+		String dbName = dotenv.get("MONGO_INITDB_DATABASE");
+
+		MongoClient mongoClient = MongoClients.create(uriDb);
+		MongoDatabase database = mongoClient.getDatabase(dbName);
+		this.collection = database.getCollection("trips");
 	}
 
 	// *** implement database operations here *** //
--- TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/Utils.java
+++ TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/Utils.java
@@ -1,5 +1,9 @@
 package ca.utoronto.utm.mcs;
 
+import com.mongodb.client.FindIterable;
+import org.bson.Document;
+import org.json.JSONArray;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,5 +15,15 @@ public class Utils {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             return br.lines().collect(Collectors.joining(System.lineSeparator()));
         }
+    }
+
+    public static JSONArray findIterableToJSONArray(FindIterable<Document> docs) throws Exception {
+        JSONArray arr = new JSONArray();
+        int i = 0;
+        for (Document doc : docs) {
+            arr.put(i, doc.toJson());
+            i++;
+        }
+        return arr;
     }
 }
